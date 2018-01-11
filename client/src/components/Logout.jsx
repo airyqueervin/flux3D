e4144619d40873eb910fceef1b4119b5ac359071
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-01-10 23:46:57 -0800

    Get projects refactored. Rendering house.

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,41 +1,60 @@
 import React, {Component} from 'react';
 
-const Logout = ({ showLogin, projects, getSelectedProject }) => (
-  <div id='container' className="ui container">
-    {console.log('projects in logout', projects)}
-    {/*<!-- header -->*/}
-    <div id='header'>
-      <div id='title'>
-        <h1>FLUX</h1>
-        <h2>AE Project</h2>
-      </div>
-      <div id='actions'>
-        <div className='select'>
-          <select onChange={getSelectedProject} className='project'>
-            <option>Please select a project</option>
-            {projects ? projects.map((project, i) => <option value={project} key={i}>{project.name}</option>) : null}
-          </select>
-        </div>
-        <div onClick={showLogin} id='logout'>logout</div>
-      </div>
-    </div>
-    {/*<!-- content-->*/}
-    <div id='content'>
-      {/*<!-- left column -->*/}
-      <div className='column'>
-        <div id='output'>
-          <div className='label'>From Flux</div>
-          <div className='select'>
-            <select className='cell'></select>
+class Logout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: ''
+    }
+  }
+
+  handleChange = (e) => {
+    console.log('Selected value in handleChange', e.target.value)
+    this.setState({selectValue: e.target.value}, () => {
+      this.props.getSelectedProject(JSON.parse(this.state.selectValue))
+    });
+  }
+
+  render() {
+    console.log('Selected value:', this.state.selectValue)
+    return (
+      <div id='container' className="ui container">
+        {console.log('projects in logout', this.props.projects)}
+        {/*<!-- header -->*/}
+        <div id='header'>
+          <div id='title'>
+            <h1>FLUX</h1>
+            <h2>AE Project</h2>
           </div>
-          {/*<!-- geometry viewport -->*/}
-          <div id='geometry'>
-            <div id='view'></div>
+          <div id='actions'>
+            <div className='select'>
+              <select value={this.state.selectValue} onChange={this.handleChange} className='project'>
+                <option value={JSON.stringify('default')}>Please select a project</option>
+                {this.props.projects ? this.props.projects.map((project, i) => <option value={JSON.stringify(project)} key={i}>{project.name}</option>) : null}
+              </select>
+            </div>
+            <div onClick={this.props.showLogin} id='logout'>logout</div>
+          </div>
+        </div>
+        {/*<!-- content-->*/}
+        <div id='content'>
+          {/*<!-- left column -->*/}
+          <div className='column'>
+            <div id='output'>
+              <div className='label'>From Flux</div>
+              <div className='select'>
+                <select className='cell'></select>
+              </div>
+              {/*<!-- geometry viewport -->*/}
+              <div id='geometry'>
+                <div id='view'></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
 
 export default Logout;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,7 +7,7 @@ import Login from './components/Login.jsx';
 import Logout from './components/Logout.jsx';
 import $ from 'jquery';
 import { box_data } from './js/box.js';
-let viewport, projects, selectedProject, projectCells, selectedOutputCell, that;
+let viewport, selectedProject, projectCells, selectedOutputCell, that;
 
 
 class App extends Component {
@@ -29,7 +29,6 @@ class App extends Component {
   }
   showLogin = () => {
     helpers.logout();
-    // $('#login').css('display', 'flex')
     this.setState({loginVisible: true});
   }
 
@@ -125,36 +124,15 @@ class App extends Component {
   getSelectedProject = (currProject) => {
     selectedProject = currProject
     console.log('GETSELECTPROJECT CALLED', selectedProject)
+    this.fetchCells()
   }
 
   fetchProjects = () => {
     that = this;
-    // get the user's projects from flux (returns a promise)
     getProjects()
       .then((data) => {
-        projects = data.entities
         console.log('data in fetchProjects:', data)
         that.setState({stateProjects: data.entities})
-        // for each project, create an option for the select box with
-        // the project.id as the value and the project.name as the label
-        // let options = projects.map(function(project) {
-        //   return $('<option>').val(project.id).text(project.name)
-        // })
-        // insert the default text as the first option
-      //   options.unshift('<option>Please select a project</option>')
-      //   // make sure the select box is empty and then insert the new options
-      //   $('select.project').empty().append(options)
-      //   // empty out the project cell (key) select boxes
-      //   $('select.cell').empty()
-      //   // attach a function to the select box change event
-      //   $('select.project').on('change', function(e) {
-      //   // find the project that was clicked on, and assign it to the global
-      //   // variable 'selectedProject'
-      //   selectedProject = projects.filter(function(p) { return p.id === e.target.value })[0]
-        
-      //   // now go fetch the project's cells (keys)
-      //   that.fetchCells()
-      // })
     })
 
   }
